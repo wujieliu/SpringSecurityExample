e5d5ea69d37f048695b095d68efa8e4ca45d5286
wujieliu <liuliuwujie@126.com>
2022-04-07 11:05:56 +0800

    1.添加自定义登陆成功跳转index

--- src/main/java/com/lolixx/example/springsecurity/controller/TestController.java
+++ src/main/java/com/lolixx/example/springsecurity/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.lolixx.example.springsecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,16 @@ public class TestController {
     @GetMapping("hello")
     public String hello() {
         return "hello spring security";
+    }
+
+    @GetMapping("index")
+    public Object index() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    //    登录成功后，便可以使用SecurityContextHolder.getContext().getAuthentication()获取到Authentication对象信息。除了通过这种方式获取Authentication对象信息外，也可以使用下面这种方式:
+    @GetMapping("index1")
+    public Object index(Authentication authentication) {
+        return authentication;
     }
 }
--- src/main/java/com/lolixx/example/springsecurity/security/MyAuthenticationSucessHandler.java
+++ src/main/java/com/lolixx/example/springsecurity/security/MyAuthenticationSucessHandler.java
@@ -3,6 +3,8 @@ package com.lolixx.example.springsecurity.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +15,15 @@ import java.io.IOException;
 
 @Component
 public class MyAuthenticationSucessHandler implements AuthenticationSuccessHandler {
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
     @Autowired
     private ObjectMapper mapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        response.setContentType("application/json;charset=utf-8");
-        response.getWriter().write(mapper.writeValueAsString(authentication));
+//        response.setContentType("application/json;charset=utf-8");
+//        response.getWriter().write(mapper.writeValueAsString(authentication));
+//        通过上面配置，登录成功后页面将跳转回引发跳转的页面。如果想指定跳转的页面，比如跳转到/index，可以将savedRequest.getRedirectUrl()修改为/index：
+        redirectStrategy.sendRedirect(request, response, "/index");
     }
 }
